c097d5321e2139f6be65ff63e9d3c13ee9389687
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2025-04-21 16:31:16 +0300

    Add files via upload

---
+++ Skillbox/3.2 Числа и арифметические операции с ними.py
@@ -0,0 +1,18 @@
+# Задача 1. Выражения
+multiplication = 6 * 39
+print(multiplication)
+exponentiation = 3**5
+print(exponentiation)
+subtraction = -8/-4
+print(subtraction)
+subtraction_sum = 10/2 +6
+print(subtraction_sum)
+# Задача 2. Эти замечательные астрологи
+a, b, c = 5, 4, 2
+print('Сложение:' ,a + b + c)
+print('Вычитание:', a - b - c)
+print('Умножение:', a * b * c)
+print('Деление:', a / b / c)
+print('Возведение в степень:', a ** b ** c)
+# Задача 3. Деление на ноль
+# print(65/0) -- ZeroDivisionError: division by zero
