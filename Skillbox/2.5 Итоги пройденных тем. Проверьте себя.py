22ed1bc3f9364f173ff09512a2ef2fecf721fceb
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2025-04-21 16:03:57 +0300

    Add files via upload

---
+++ Skillbox/2.5 Итоги пройденных тем. Проверьте себя.py
@@ -0,0 +1,37 @@
+# Задача 1. Найти пропавшую переменную
+client = 'Петя'
+pet = 'Кот'
+print(client)
+print(' и ')
+print(pet)
+# Задача 2. Цвета
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+print(r, b, g, r + g + b, b, g + b)
+# Задача 3. Животные
+first_animal = 'Заяц'
+second_animal = 'Черепаха'
+print(first_animal, 'спит,', second_animal, 'идёт')
+# Задача 4. Вход в систему
+first_name = input('Введите имя пользователя: ')
+greeting = 'Привет,'
+print(greeting, first_name)
+intro = "К сожалению, у Вас нет доступа к системе."
+info = "Пожалуйста, обратитесь к системному администратору."
+print(intro)
+print(info)
+# Задача 5. Полёт
+start = input('Город вылета: ')
+final = input('Город прилета: ')
+ticket = start + " - " + final
+print(ticket)
+# Задача 6. Обмен значений двух переменных
+a = input('Введите первое слово: ')
+b = input('Введите второе слово: ')
+print(a, b)
+z = a
+c = b
+b = z
+a = c
+print(a, b)
